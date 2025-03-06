9aa610f23304424e6668ca720e6d95bdbd2f1517
JostMK <jost.klink@gmail.com>
2025-03-06 20:06:22 +0100

    testing to use more than just headers
    On windows only works if gdal.dll and its dependencies are copied next to libGeoRasters.dll.
    This is undesired because it would mean that the gdextension also needs to ship all those dependencies.
    
    Might after all need to switch to manually compile gdal to have it fully statically compiled into libGeoRasters.dll.

---
+++ geoimportertest/rasters/read_me.txt
@@ -0,0 +1,13 @@
+Datum des Downloads: 2025-01-07T22:47:36
+Data di scarico: 2025-01-07T22:47:36
+Download date: 2025-01-07T22:47:36
+
+
+
+LIZENZ - LICENZA - LICENSE
+
+Die Autonome Provinz Bozen - Südtirol verwendet die Lizenz “Creative Commons Zero (CC0)“: https://creativecommons.org/publicdomain/zero/1.0/deed.de
+
+La Provincia Autonoma di Bolzano - Alto Adige fa uso della licenza "Creative Commons  Zero (CC0)": https://creativecommons.org/publicdomain/zero/1.0/deed.it
+
+The Autonomous Province of Bolzano - South Tyrol uses the license "Creative Commons Zero (CC0)": https://creativecommons.org/publicdomain/zero/1.0/deed.en
--- src/geo_raster.cpp
+++ src/geo_raster.cpp
@@ -5,6 +5,8 @@
 #include "geo_raster.h"
 #include "gdal_version.h"
 
+#include "gdal_priv.h" // GDAL C++ API
+
 namespace georasters
 {
     void GeoRaster::_bind_methods()
@@ -14,6 +16,23 @@ namespace georasters
     GeoRaster::GeoRaster()
     {
         godot::UtilityFunctions::print_rich("Using GDAL ", GDAL_RELEASE_NAME);
+        GDALAllRegister();
+
+        const auto filepath = R"(./rasters/DigitalTerrainModel-0.5m.tif)";
+        const auto pDataset =
+            GDALDatasetUniquePtr(GDALDataset::FromHandle(GDALOpen(filepath, GA_ReadOnly)));
+
+        if (!pDataset)
+        {
+            godot::UtilityFunctions::push_error("Failed to open dataset!");
+            return;
+        }
+
+        const auto size_x = pDataset->GetRasterXSize();
+        const auto size_y = pDataset->GetRasterYSize();
+        const auto band_count = pDataset->GetRasterCount();
+        godot::UtilityFunctions::print_rich("Opened dataset: dimensions: (", size_x, ", ", size_y,
+                                            "), bands: ", band_count);
     }
 
     GeoRaster::~GeoRaster() = default;
